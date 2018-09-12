23f4618d46b05655ad22c3065a738317173465b3
Dean Nellis <nellisdean@gmail.com>
2018-09-12 20:56:50 +0000

    Added Redux developer tools extension line

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,7 +5,8 @@ export default () => {
     const store = createStore(
         combineReducers({
             characters: charactersReducer
-        })    
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     
     return store;
